ee359d451594a8167084d8236aa57c23f1a07030
smarun16 <arunprasad.s@angleritech.com>
2017-06-03 10:47:31 +0530

    test

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func setupUI() {
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
 
 }
 
